7de641f247ae64c6c42afc60ce6c855c7f5e24b8
cyber2100 <a@ua-pc.localdomain>
2025-05-22 10:56:11 -0400

    fixed: dashboard transaactio data changed

--- src/hooks/use-auth.tsx
+++ src/hooks/use-auth.tsx
@@ -196,7 +196,9 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
 
   const value = {
     user,
-    isAuthenticated: !!user,
+    isAuthenticated: 
+      // true, 
+      !!user,
     isLoading,
     signin,
     signup,
